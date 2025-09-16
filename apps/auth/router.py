e7e5779957d249f8cbd879a9a6fb298cac182954
Aswanth Vc <aswanth.abam@gmail.com>
2025-09-17 00:49:39 +0530

    fix(auth): update login api request validation

--- apps/auth/router.py
+++ apps/auth/router.py
@@ -13,8 +13,8 @@ router = APIRouter(
 @router.post("/login")
 async def login(
     auth_service: AuthServiceDependency,
-    username: str = Body(...),
-    password: str = Body(...),
+    username: str = Body(..., min_length=5, max_length=100, regex=r"^[A-Za-z0-9_.-]+$"),
+    password: str = Body(..., min_length=5, max_length=100),
 ):
     token = auth_service.authenticate_and_create_jwt(username, password)
     return {"access_token": token}
